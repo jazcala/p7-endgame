46ee54fce8a0c2bcfae62f6e22792bc229467855
jazcala <jazcalabrese@gmail.com>
2025-03-13 23:47:07 -0300

    Adding Header Component, styles and Hanken Grotesk font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,11 @@
 export default function Header() {
-  return <h1>Assembly: Endgame</h1>;
+  return (
+    <header>
+      <h1>Assembly: Endgame</h1>
+      <p>
+        Guess the word in under 8 attempts to keep the programming world safe
+        from Assembly!
+      </p>
+    </header>
+  );
 }
