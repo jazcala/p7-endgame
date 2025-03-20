68151f2cf9fb512270f2d752d22cf0d9a3f50075
jazcala <jazcalabrese@gmail.com>
2025-03-20 09:32:53 -0300

    Adding footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
